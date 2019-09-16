ca11143f3b7f0aeada1f0e7a216565a835e5d86a
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-16 12:56:07 -0300

    Página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboradType="email-address"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Digite ser e-email"
+          />
+          <FormInput
+            icon="lock-outline"
+            keyboradType="email-address"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Digite ser e-email"
+          />
 
-      <Button>Entrar</Button>
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Form = styled.View``;
+
+export const FormInput = styled.View``;
+
+export const SubmitButton = styled.View``;
+
+export const SignLink = styled.View``;
+
+export const SignLinkText = styled.View``;
