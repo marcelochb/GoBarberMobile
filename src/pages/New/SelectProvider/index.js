8e9457efa76e87409e3ba5dfd9c6ecf564926927
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-17 23:07:22 -0300

    Listagem de prestador

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
 export default function SelectProvider() {
-  return <Background />;
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  });
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider>
+              <Avatar
+                source={{
+                  uri: provider.avatar.url
+                    ? provider.avatar.url
+                    : `http://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
