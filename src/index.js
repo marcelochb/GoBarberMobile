44104d68a3937c6492585a89d27caa5995202a3e
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-27 22:08:24 -0300

    Enviando notificação push

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 import App from './App';
 
-const Index = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <App />
-      </PersistGate>
-    </Provider>
-  );
-};
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('e5936c55-7149-4920-a597-48da4d8ade95');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+  onOpened = notification => {};
+  onIds = id => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 export default CodePush({
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
